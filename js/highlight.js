81976e4c3d3650472bb999087727c0b4c4376db5
lbiester <laurajpb@gmail.com>
2014-12-14 20:31:34 -0500

    Window location seems to be a bit off in firefox, this makes that less noticeable

--- js/highlight.js
+++ js/highlight.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 	$(window).scroll(function() {
-			if (window.pageYOffset < $('div#about').offset().top) {
+			if (window.pageYOffset < $('div#about').offset().top - 5) {
 				$('a.selected').removeClass('selected');
 				$('a#home-link').addClass('selected');
-			} else if (window.pageYOffset < $('div#skills').offset().top) {
+			} else if (window.pageYOffset < $('div#skills').offset().top - 5) {
 				$('a.selected').removeClass('selected');
 				$('a#about-link').addClass('selected');
-			} else if (window.pageYOffset < $('div#contact').offset().top) {
+			} else if (window.pageYOffset < $('div#contact').offset().top - 5) {
 				$('a.selected').removeClass('selected');
 				$('a#skills-link').addClass('selected');
 				if ($(window).width() < 768 && !colorFadedIn) {
