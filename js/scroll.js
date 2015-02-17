0a1c36b78866e17816f47858bcffc5cc01c40319
lbiester <laurajpb@gmail.com>
2015-02-16 23:52:07 -0600

    Add ga for clicking on navigation

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $('a.nav-link').click(function() {
         var target = $($(this).attr('data-div'));
+        ga('send', 'event', 'link', 'click', 'nav links' + target);
         $('html, body').animate({
             scrollTop: target.offset().top
         }, 1000);
